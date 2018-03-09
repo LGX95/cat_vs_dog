a90ca4f1ba242df66dd69c33d38e0c5b32bb41ea
LGX95 <ligengxin95@gmail.com>
2018-03-09 09:44:02 +0800

    编写训练和验证的代码

--- main.py
+++ main.py
@@ -9,9 +9,37 @@ __author__ = 'LGX95'
 
 import os
 
+import torch.nn as nn
+import torch.optim as optim
 import torch.utils.data as Data
 import torchvision.datasets as datasets
+import torchvision.models as models
 import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+from utils import AverageMeter, accuracy
+
+
+def main():
+    train_loader, val_loader = load_data('./datasets/cat_vs_dog/')
+
+    print("=> creating model ")
+    model = models.resnet152(pretrained=True)
+    # 冻结模型的参数
+    for param in model.parameters():
+        param.requires_grad = False
+    in_features = model.fc.in_features
+    # 替换全连接层，新构造的模块的 requires_grad 为 True
+    model.fc = nn.Linear(in_features, 2)
+
+    # 定义损失函数
+    criterion = nn.CrossEntropyLoss()
+    # 定义优化器
+    optimizer = optim.Adam(model.fc.parameters(), 1e-3)
+
+    for epoch in range(1):
+        train(train_loader, model, criterion, optimizer, epoch)
+        prec1 = validate(val_loader, model, criterion)
 
 
 def load_data(data_dir):
@@ -53,7 +81,90 @@ def load_data(data_dir):
         train_dataset, batch_size=32, shuffle=True
     )
     val_loader = Data.DataLoader(
-        val_dataset, batch_size=32, shuffle=False, 
+        val_dataset, batch_size=32, shuffle=False
     )
 
     return train_loader, val_loader
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    """训练
+
+    Args:
+        train_loader: 训练数据集的 DataLoader
+        model: CNN 模型
+        criterion: 误差函数
+        optimizer: 优化器
+        epoch: 当前训练的轮数
+    """
+    # model switch to train mode
+    model.train()
+
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    for i, (image, target) in enumerate(train_loader):
+        # target = target.cuda(async=True)
+        image_var, target_var = Variable(image), Variable(target)
+
+        # 前馈运算和计算损失
+        output = model(image_var)
+        loss = criterion(output, target_var)
+
+        # 记录准确率和损失
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.data[0], image.size(0))
+        top1.update(prec1[0], image.size(0))
+
+        # 反向传播和更新权值
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print('Epoch: [{0}][{1}/{2}]'
+              ' | Loss: {loss.val:.4f} ({loss.avg:.4f})'
+              ' | top1: {top1.val:.3f} ({top1.avg:.3f})'.format(
+                  epoch, i, len(train_loader), loss=losses, top1=top1))
+
+
+def validate(val_loader, model, criterion):
+    """验证
+
+    Args:
+        val_loader: 验证数据集的 DataLoader
+        model: CNN 模型
+        criterion: 误差函数
+
+    Return:
+        测试准确率的平均值
+    """
+    # model switch to evaluate mode
+    model.eval()
+
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    for i, (image, target) in enumerate(val_loader):
+        # target = target.cuda(async=True)
+        image_var = Variable(image, volatile=True)
+        target_var = Variable(target, volatile=True)
+
+        # 前馈运算和计算损失
+        output = model(image_var)
+        loss = criterion(output, target_var)
+
+        # 记录准确率和损失
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.data[0], image.size(0))
+        top1.update(prec1[0], image.size(0))
+
+        print('Validata: [{0}/{1}]'
+              ' | Loss: {loss.val:.4f} ({loss.avg:.4f})'
+              ' | Accuracy: {top1.val:.3f} ({top1.avg:.3f})'.format(
+                  i, len(val_loader), loss=losses, top1=top1))
+
+    return top1.avg
+
+
+if __name__ == '__main__':
+    main()
